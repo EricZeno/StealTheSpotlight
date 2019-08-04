76648cde08f08e26148e9b24ffa23a207695f3fb
Tom Bar Ezer Ayalon <toba1020@berkeley.edu>
2019-08-03 18:10:33 -0700

    hotfix stuff

--- Assets/Scripts/Player/PlayerWeapon.cs
+++ Assets/Scripts/Player/PlayerWeapon.cs
@@ -3,14 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public interface PlayerWeaponInterface {
-    void UnsetWeapon();
-    void SetWeapon(int weaponID);
+    void Unequip();
+    void Equip(int weaponID);
 
-    void UseWeapon();
+    void Use();
+    void StopUse();
 }
 
 [DisallowMultipleComponent]
 public class PlayerWeapon : MonoBehaviour, PlayerWeaponInterface {
+    #region Private Variables
+    private int p_Damage;
+
+    private int p_AttackSpeed;
+
+    private Sprite p_Sprite;
+
+    private float p_WindupTime;
+
+    private float p_AnimationLength;
+    #endregion
+
+    #region Interface Required Methods
+    public void Equip(int weaponID) {
+        throw new System.NotImplementedException();
+    }
+
+    public void Unequip() {
+        throw new System.NotImplementedException();
+    }
+
+    public void Use() {
+        throw new System.NotImplementedException();
+    }
+
+    public void StopUse() {
+        throw new System.NotImplementedException();
+    }
+    #endregion
 
     #region Input Receivers
     // Executes attack functionality when player attack input is received

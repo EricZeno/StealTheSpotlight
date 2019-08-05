9c503c8c5c6389cac138861c2a5ec14b5acf6229
Tom Bar Ezer Ayalon <toba1020@berkeley.edu>
2019-08-05 02:23:17 -0700

    single weapon works

---
+++ Assets/Scripts/Items/Weapons/WeaponBase.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct WeaponBaseData {
+    #region Private Variables
+    private float p_Damage;
+
+    private float p_KnockbackPower;
+
+    private float p_AttackSpeed;
+
+    private Sprite p_Sprite;
+    #endregion
+
+    #region Constructors
+    public WeaponBaseData(float damage, float knockbackPower, float attackSpeed, Sprite sprite) {
+        p_Damage = Mathf.Max(0, damage);
+        p_KnockbackPower = Mathf.Max(0, knockbackPower);
+        p_AttackSpeed = Mathf.Max(Consts.MINIMUM_ATTACK_SPEED, attackSpeed);
+        p_Sprite = sprite;
+    }
+    #endregion
+
+    #region Accessors and Mutators
+    public float GetDamage() {
+        return p_Damage;
+    }
+
+    public float GetKnockbackPower() {
+        return p_KnockbackPower;
+    }
+
+    public float GetAttackSpeed() {
+        return p_AttackSpeed;
+    }
+
+    public Sprite GetSprite() {
+        return p_Sprite;
+    }
+    #endregion
+}
+
+public interface WeaponInterface {
+    void Activate(WeaponBaseData data);
+    void Deactivate();
+    void UpdateGraphics(WeaponBaseData data);
+}
+
+[DisallowMultipleComponent]
+public class WeaponBase : MonoBehaviour, WeaponInterface
+{
+    #region Cached Reference
+    private SpriteRenderer cr_Renderer;
+
+    private WeaponHitbox cr_Hitbox;
+    #endregion
+
+    #region Initialization
+    private void Start() {
+        cr_Renderer = GetComponentInChildren<SpriteRenderer>();
+        if (cr_Renderer == null) {
+            Debug.LogError("Could not find sprite renderer.");
+        }
+
+        cr_Hitbox = GetComponentInChildren<WeaponHitbox>();
+        if (cr_Hitbox == null) {
+            Debug.LogError("Could not find weapon collider.");
+        }
+    }
+    #endregion
+
+    #region Interface Required Methods
+    public void Activate(WeaponBaseData data) {
+        cr_Hitbox.gameObject.SetActive(true);
+        cr_Hitbox.SetHitboxStats(data);
+    }
+
+    public void Deactivate() {
+        cr_Hitbox.gameObject.SetActive(false);
+    }
+
+    public void UpdateGraphics(WeaponBaseData data) {
+        cr_Renderer.sprite = data.GetSprite();
+    }
+    #endregion
+}
--- Assets/Scripts/Player/PlayerWeapon.cs
+++ Assets/Scripts/Player/PlayerWeapon.cs
@@ -11,34 +11,200 @@ public interface PlayerWeaponInterface {
 }
 
 [DisallowMultipleComponent]
+[RequireComponent(typeof(Animator))]
 public class PlayerWeapon : MonoBehaviour, PlayerWeaponInterface {
     #region Private Variables
-    private int p_Damage;
+    private bool p_HasWeaponEquipped;
+    private int p_WeaponID;
+    private bool p_IsInUse;
+    private float p_CooldownTimer;
 
-    private int p_AttackSpeed;
-
-    private Sprite p_Sprite;
-
+    private bool p_IsInAnimation;
     private float p_WindupTime;
-
+    private float p_AttackTime;
     private float p_AnimationLength;
+
+    private WeaponBaseData p_WeaponBaseData;
+    #endregion
+
+    #region Cached Components
+    private Animator cc_Animator;
+    #endregion
+
+    #region Cached References
+    private WeaponInterface cr_Weapon;
+    #endregion
+
+    #region Initialization
+    private void Awake() {
+        cc_Animator = GetComponent<Animator>();
+
+        p_HasWeaponEquipped = false;
+        p_WeaponID = -1;    // TODO: Set to not a weapon
+        p_IsInUse = false;
+        p_IsInAnimation = false;
+    }
+
+    private void Start() {
+        cr_Weapon = GetComponentInChildren<WeaponInterface>();
+        if (cr_Weapon == null) {
+            Debug.LogError("Could not find WeaponBase.");
+        }
+        cr_Weapon.Deactivate();
+    }
+    #endregion
+
+    #region Main Updates
+    private void Update() {
+        // Mimicing controller
+        ControllerUpdate();
+
+        //--------------------
+        if (p_IsInUse && CanUse()) {
+            StartCoroutine(UseWeapon());
+        }
+        else if (WaitingOnCooldown()) {
+            p_CooldownTimer -= Time.deltaTime;
+        }
+        else {
+            p_CooldownTimer = 0;
+        }
+    }
+    #endregion
+
+    #region Secondary Updates
+    private void ControllerUpdate() {
+        if (Input.GetButtonDown(Consts.TEST_BUTTON_2)) {
+            if (p_HasWeaponEquipped) {
+                Unequip();
+            }
+            else {
+                Equip(0);
+            }
+        }
+
+        if (Input.GetButtonDown(Consts.TEST_BUTTON_3)) {
+            if (p_HasWeaponEquipped) {
+                Unequip();
+            }
+            else {
+                Equip(1);
+            }
+        }
+
+        if (Input.GetButtonDown(Consts.TEST_BUTTON_1)) {
+            Use();
+        }
+        else if (Input.GetButtonUp(Consts.TEST_BUTTON_1)) {
+            StopUse();
+        }
+    }
+    #endregion
+
+    #region Resetters
+    private void ResetCooldown() {
+        p_CooldownTimer = Consts.ATTACK_SPEED_MULTIPLIER * 1 / p_WeaponBaseData.GetAttackSpeed();
+    }
+    #endregion
+
+    #region Checkers
+    private bool CanUse() {
+        if (!p_HasWeaponEquipped)
+            return false;
+        if (p_IsInAnimation)
+            return false;
+        if (p_CooldownTimer > 0)
+            return false;
+
+        return true;
+    }
+
+    private bool WaitingOnCooldown() {
+        return p_CooldownTimer > 0;
+    }
+    #endregion
+
+    #region Equip Helper Methods
+    private void SetWeaponData() {
+        string type = "ShortBlade";
+        p_WindupTime = WeaponTypeDataManager.singleton.WindupTime(type);
+        p_AttackTime = WeaponTypeDataManager.singleton.AttackTime(type);
+        p_AnimationLength = WeaponTypeDataManager.singleton.AnimationTime(type);
+
+        // TODO: set rest of data
+        if (p_WeaponID == 0) {
+            p_WeaponBaseData = new WeaponBaseData(0, 0, 1, Utility.LoadSpriteFile(Consts.TEST_SWORD_1_SPRITE_PATH));
+        }
+        else if (p_WeaponID == 1) {
+            p_WeaponBaseData = new WeaponBaseData(0, 0, 1, Utility.LoadSpriteFile(Consts.TEST_SWORD_2_SPRITE_PATH));
+        }
+    }
+
+    private void ApplyWeaponData() {
+        cr_Weapon.UpdateGraphics(p_WeaponBaseData);
+    }
+    #endregion
+
+    #region Use Methods
+    private IEnumerator UseWeapon() {
+        StartAnimations();
+
+        yield return new WaitForSeconds(p_WindupTime);
+
+        ActivateWeapon();
+
+        yield return new WaitForSeconds(p_AttackTime - p_WindupTime);
+
+        DeactivateWeapon();
+
+        yield return new WaitForSeconds(p_AnimationLength - p_AttackTime);
+
+        EndAnimations();
+
+        ResetCooldown();
+    }
+
+    private void StartAnimations() {
+        p_IsInAnimation = true;
+
+        cc_Animator.SetTrigger(Consts.USE_WEAPON_ANIMATOR_TRIGGER);
+    }
+
+    private void EndAnimations() {
+        p_IsInAnimation = false;
+    }
+
+    private void ActivateWeapon() {
+        cr_Weapon.Activate(p_WeaponBaseData);
+    }
+
+    private void DeactivateWeapon() {
+        cr_Weapon.Deactivate();
+    }
     #endregion
 
     #region Interface Required Methods
     public void Equip(int weaponID) {
-        throw new System.NotImplementedException();
+        p_WeaponID = weaponID;
+
+        // TODO: Get weapon data
+        // TODO: Set weapon data
+        SetWeaponData();
+        ApplyWeaponData();
+        
+        p_HasWeaponEquipped = true;
     }
 
     public void Unequip() {
-        throw new System.NotImplementedException();
+        p_HasWeaponEquipped = false;
     }
 
     public void Use() {
-        throw new System.NotImplementedException();
+        p_IsInUse = true;
     }
 
     public void StopUse() {
-        throw new System.NotImplementedException();
+        p_IsInUse = false;
     }
     #endregion
 

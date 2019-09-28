a0704507e6cb5e64cf5c5eb57a7476a1867d196c
Tom Bar Ezer Ayalon <toba1020@berkeley.edu>
2019-09-28 13:09:57 -0700

    Adding Testing for Items
    Adding in a player prefab that can be used for testing.<br>
    Added a way to apply damage to player in game so items affecting player
    health can be tested.
    
    Changelog:
    * Added Testing directory
    * Added Testing/Prefabs directory
    * Added Testing/Scripts directory
    * Created a PlayerTesting prefab to use in order to test the items
    * Finished ApplyDamageToPlayer: given a player and an amount of damage
    to deal, user can press the button in the inspector to deal damage to
    the player while the game is running

---
+++ Assets/Testing/Scripts/Misc/ApplyDamageToPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyDamageToPlayer : MonoBehaviour
+{
+    #region Editor Variables
+    [SerializeField]
+    [Tooltip("The player to apply damage to.")]
+    private PlayerManager m_Player;
+
+    [SerializeField]
+    [Tooltip("The amount of damage to apply")]
+    private int m_Damage;
+    #endregion
+
+    #region Apply Damage Methods
+    public void ApplyDamage() {
+        m_Player.TakeDamage(m_Damage);
+    }
+    #endregion
+}
---
+++ Assets/Testing/Scripts/Player/PlayerMovementTesting.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovementTesting : MonoBehaviour
+{
+    #region Instance Variables
+    private Vector2 p_MoveDir;
+    #endregion
+
+    #region Cached Components
+    private PlayerManager cc_Manager;
+    private Rigidbody2D cc_Rb;
+    #endregion
+
+    #region Initialization
+    private void Awake() {
+        cc_Manager = GetComponent<PlayerManager>();
+        cc_Rb = GetComponent<Rigidbody2D>();
+    }
+    #endregion
+
+    #region Main Updates
+    private void Update() {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        SetMoveDir(x, y);
+    }
+
+    private void FixedUpdate() {
+        Move();
+    }
+    #endregion
+
+    #region Movement Methods
+    private void SetMoveDir(float x, float y) {
+        // TODO: Error checking on x and y
+        p_MoveDir = new Vector2(x, y);
+        if (p_MoveDir.sqrMagnitude > 1) {
+            p_MoveDir.Normalize();
+        }
+    }
+
+    private void Move() {
+        cc_Rb.MovePosition(cc_Rb.position + p_MoveDir * cc_Manager.GetMoveSpeed() * Time.fixedDeltaTime);
+    }
+    #endregion
+}

097b97fef0bbf2840598bc4942c9a3a0f9ffa499
unknown <jazhang@berkeley.edu>
2019-12-05 15:50:29 -0800

    Added green and blue axes

--- Assets/Scripts/Items/Base/BaseWeaponItem.cs
+++ Assets/Scripts/Items/Base/BaseWeaponItem.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public enum WeaponTypes {
     DAGGER,
-    POLEARM
+    POLEARM,
+    HEAVY
 }
 
 public abstract class BaseWeaponItem : BaseItem {
---
+++ Assets/Scripts/Items/Weapons/Heavy/Heavy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Heavy", menuName = "Items/Weapons/Heavy")]
+public class Heavy : BaseWeaponItem {
+    #region Initialization
+    protected override void Awake() {
+        m_WeaponType = WeaponTypes.HEAVY;
+
+        m_NumRaycasts = 10;
+
+        m_RightOffset = new Vector2(1f, -.2f);
+        m_IdleAngleOffset = -90;
+
+        m_AttackAngleOffset = -70;
+        m_WindupPercent = .1f;
+        m_AttackAnimationPercent = .3f;
+        m_AnimationResetPercent = .3f;
+    }
+    #endregion
+}

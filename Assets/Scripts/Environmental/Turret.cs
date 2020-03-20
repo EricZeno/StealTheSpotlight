7345ce0bbbcd75a7d02c76794308dd0b47f84cc2
awc71403 <awc71403@gmail.com>
2020-03-19 17:08:20 -0700

    Added 5 environmentals
    Changelog:
    - Added two new tags Pot and Bush
    - Added Prefabs for each using the Spike sprite as their placeholder
    
    Bugs:
    - Can't do quaternion for the life of me for the turret
    - Rocks don't blow up yet because no explosion
    - Slight bug with spikes where if they activate and you don't move, then
    you don't take damage

---
+++ Assets/Scripts/Environmental/Turret.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    #region Editor Variables
+    [SerializeField]
+    [Tooltip("How much damage do you take")]
+    private int m_damage;
+
+    [SerializeField]
+    [Tooltip("How much time between burst of shots")]
+    private float m_interval;
+
+    [SerializeField]
+    [Tooltip("How much time to shoot X bullets")]
+    private float m_timeframe;
+
+    [SerializeField]
+    [Tooltip("How much bullets per burst")]
+    private int m_bullets;
+
+    [SerializeField]
+    [Tooltip("Degree to shoot at")]
+    private int m_degree;
+
+    [SerializeField]
+    [Tooltip("The bullet prefab")]
+    private GameObject m_bullet;
+
+    [SerializeField]
+    [Tooltip("How fast the bullets will travel")]
+    private float m_shotspeed;
+    #endregion
+
+    #region Private Variable
+    private float m_timer;
+    #endregion
+
+    #region Initialization
+    private void Start() {
+        m_timer = m_interval;
+    }
+    #endregion
+
+    #region Update
+    void Update() {
+        if (m_timer > 0) {
+            m_timer -= Time.deltaTime;
+        }
+        else {
+            m_timer = m_interval;
+            StartCoroutine(Shoot());
+        }
+    }
+    #endregion
+
+    #region Shoot
+    private IEnumerator Shoot() {
+        float timeBetweenShots = m_bullets / m_timeframe;
+        Vector2 firingVector = Vector2.up;
+        //Fix direction
+        firingVector = Quaternion.Euler(0, 0, m_degree) * firingVector;
+        firingVector.Normalize();
+
+        for (int i = 0; i < m_bullets; i++) {
+            GameObject bullet = Instantiate(m_bullet, transform);
+            bullet.GetComponent<Projectile>().Setup(m_damage, firingVector, m_shotspeed);
+
+            yield return new WaitForSeconds(timeBetweenShots);
+        }
+        yield return null;
+    }
+    #endregion
+}
--- Assets/Scripts/Player/PlayerWeaponCollision.cs
+++ Assets/Scripts/Player/PlayerWeaponCollision.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class PlayerWeaponCollision : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log("Triggered");
         if (collision.gameObject.layer == transform.parent.gameObject.layer) {
             return;
         }
 
         if (collision.CompareTag(Consts.PLAYER_TAG) || collision.CompareTag(Consts.GENERAL_ENEMY_TAG)) {
             GetComponentInParent<PlayerWeapon>().HitEnemy(collision.gameObject);
+        }
+
+        if (collision.CompareTag(Consts.BUSH_PHYSICS_LAYER)) {
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.CompareTag(Consts.POT_PHYSICS_LAYER)) {
+            Destroy(collision.gameObject);
         }
     }
 }

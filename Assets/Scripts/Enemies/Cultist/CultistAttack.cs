f77a3f6ee977a747260bae84036b7c6b6354d740
Eric McCormick (1516104) <eric.mccormick@berkeley.edu>
2020-04-12 16:23:36 -0700

    Cultist (#122)

---
+++ Assets/Scripts/Enemies/Cultist/CultistAttack.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CultistAttack : EnemyAttack {
+    #region Private Variables
+    private bool m_AttackLocked;
+    private int m_Damage;
+    #endregion
+
+    #region Editor Variables
+    [SerializeField]
+    [Tooltip("The rune gameobject")]
+    private GameObject m_Rune;
+
+    [SerializeField]
+    [Tooltip("How long it takes for the rune to do damage")]
+    private float m_CastTime;
+
+    [SerializeField]
+    [Tooltip("Radius of the rune")]
+    private float m_RuneRadius;
+    #endregion
+
+    #region Initialization
+    private void Awake() {
+        base.Awake();
+        m_Damage = m_Manager.GetEnemyData().Damage;
+        m_Rune.transform.localScale = new Vector3(m_RuneRadius, m_RuneRadius, 1);
+    }
+    #endregion
+
+    #region Attack
+    public override void Attack(int attackNum, Vector2 target) {
+        if (!m_AttackLocked) {
+            StartCoroutine(Attack(target));
+        }
+    }
+
+    private IEnumerator Attack(Vector2 target) {
+        m_AttackLocked = true;
+        GameObject rune = Instantiate(m_Rune, target, Quaternion.identity);
+        rune.transform.position = target;
+        rune.GetComponent<RuneDestroyer>().time = m_CastTime;
+        rune.SetActive(true);
+        yield return new WaitForSeconds(m_CastTime);
+
+        RaycastHit2D[] targets = Physics2D.CircleCastAll(rune.transform.position, m_RuneRadius / 10, Vector3.up, 0.01f);
+        foreach (RaycastHit2D hit in targets) {
+            if (hit.collider.gameObject.tag == Consts.PLAYER_TAG) {
+                PlayerManager player = hit.collider.gameObject.GetComponent<PlayerManager>();
+                player.TakeDamage(m_Damage);
+            }
+        }
+
+        Destroy(rune);
+        m_AttackLocked = false;
+        ((CultistMovement)m_Movement).Attacking = false;
+    }
+    #endregion
+
+}
---
+++ Assets/Scripts/Enemies/Cultist/CultistMovement.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CultistMovement : EnemyMovement {
+    #region Private Variables
+    private Vector2 currVec;
+    private bool m_Moving;
+    private float m_MoveTime;
+    private bool m_Attacking;
+    public bool Attacking {
+        set {
+            m_Attacking = value;
+        }
+    }
+    private float m_CurrMoveTime;
+    private float m_Range;
+    #endregion
+
+    #region Initialization
+    protected override void Awake() {
+        base.Awake();
+        m_MoveTime = 1f / m_Manager.GetEnemyData().AttackSpeed;
+        m_Range = m_Manager.GetEnemyData().AttackRange;
+    }
+    #endregion
+
+    #region Movement
+    protected override void MovementAlgorithm() {
+        base.MovementAlgorithm();
+        if (m_Attacking) {
+            return;
+        }
+
+        if (m_Moving) {
+            Move();
+
+            m_CurrMoveTime -= Time.fixedDeltaTime;
+
+            if (m_CurrMoveTime <= 0) {
+                m_Moving = false;
+
+            }
+            else {
+                return;
+            }
+        }
+
+        GameObject target = FindClosestTarget();
+        if (target != null) {
+            Vector2 targetPos = target.transform.position;
+
+            if (InRange(targetPos)) {
+                m_Attacking = true;
+                ((CultistAttack)m_Attack).Attack(0, targetPos);
+
+                CalculateMove();
+            }
+        }
+        else {
+            CalculateMove();
+        }
+    }
+
+    private void CalculateMove() {
+        Unfreeze();
+
+        if (m_CurrMoveTime <= 0) {
+            Vector2 randomVec = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+            currVec = randomVec;
+            SetMove(currVec);
+        }
+        else {
+            SetMove(currVec);
+        }
+        m_Moving = true;
+        m_CurrMoveTime = m_MoveTime;
+    }
+    #endregion
+
+    #region Range Check
+    private bool InRange(Vector2 target) {
+        float distance = Vector2.Distance(target, transform.position);
+        return distance <= m_Range;
+    }
+    #endregion
+
+    #region Collision
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (m_Moving && collision.gameObject.layer == LayerMask.NameToLayer("Wall")
+             || collision.gameObject.layer == LayerMask.NameToLayer("Object")) {
+            m_Moving = false;
+            m_CurrMoveTime = m_MoveTime;
+            FreezePosition();
+        }
+    }
+    #endregion
+
+    #region Stopping Pushes
+    private void FreezePosition() {
+        m_RB.constraints = RigidbodyConstraints2D.FreezeAll;
+    }
+
+    private void Unfreeze() {
+        m_RB.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+    #endregion
+}
